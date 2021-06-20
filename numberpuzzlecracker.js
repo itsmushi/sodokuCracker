29c6a31e02eeb8b2910d75eedbc4ec11a74d277b
mushi <jmushi175@gmail.com>
2021-06-21 00:59:19 +0300

    progress

--- numberpuzzlecracker.js
+++ numberpuzzlecracker.js
@@ -34,7 +34,7 @@ function givePosibleSoln(){
     
     allPossible=[];
     row=[];
-    
+
 
 
 
@@ -76,6 +76,30 @@ function givePosibleSoln(){
 // function giveArraysOfCubes(sampleSolnArray){
 
 // }
+
+function givePossibleRow(){
+    allRows=[];
+    temp=[];
+    for(a=1;a<10;a++){
+        for(b=1;b<10;b++){
+            for(c=1;c<10;c++){
+                for(d=1;d<10;d++){
+                    for(e=1;e<10;e++){
+                        for(f=1;f<10;f++){
+                            for(g=1;g<10;g++){
+                                for(h=1;h<10;h++){
+                                    for(i=1;i<10;i++){
+                                        
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
 
 
 
